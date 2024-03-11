25a81f4318ad4db040547dfd3a19b393246753c7
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-03-11 15:27:36 +0530

    Changed folders

---
+++ com/inbravo/sqls/lms-sql.sql
@@ -0,0 +1,8 @@
+create database db_librarymanagement;
+
+CREATE TABLE table_publisher (
+PublisherName VARCHAR(50) PRIMARY KEY NOT NULL,
+PublisherAddress VARCHAR(100) NOT NULL,
+PublisherPhone VARCHAR(20) NOT NULL);
+
+
