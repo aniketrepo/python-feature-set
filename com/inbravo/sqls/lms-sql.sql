569aec9fbbc0ba7e04f552f4f83485734ed49c88
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-03-14 19:28:34 +0530

    printing hello world in python

--- com/inbravo/sqls/lms-sql.sql
+++ com/inbravo/sqls/lms-sql.sql
@@ -1,8 +1,2 @@
 create database db_librarymanagement;
 
-CREATE TABLE table_publisher (
-PublisherName VARCHAR(50) PRIMARY KEY NOT NULL,
-PublisherAddress VARCHAR(100) NOT NULL,
-PublisherPhone VARCHAR(20) NOT NULL);
-
-
