fc7d51e1193830591acdefae3205445b8f2418d0
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-03-14 19:51:07 +0530

    using print modifiers

--- com/inbravo/100-days-of-coding/using print modifiers.py
+++ com/inbravo/100-days-of-coding/using print modifiers.py
@@ -1,3 +1,3 @@
 print('She said: "Hello" and then left.')   # double quote in single quote
 print("She said: 'Hello' and then left.")   # single quote in double quote
-print("She said: \"Hello\" and then left.")
+print("She said: \"Hello\" and then left.") # escaping the quote
