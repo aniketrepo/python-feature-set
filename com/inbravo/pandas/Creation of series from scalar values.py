86133224cf773af78e734e2f207e8626fe87ecfa
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-03-19 11:55:08 +0530

    using print modifiers

--- com/inbravo/pandas/Creation of series from scalar values.py
+++ com/inbravo/pandas/Creation of series from scalar values.py
@@ -1,4 +1,7 @@
 import pandas as pd
 
-series= pd.Series([10,20,30,40])
-print(series)
+series1= pd.Series([10,20,30,40])   # Unspecified index
+print(series1)
+
+series2= pd.Series(['Jake','Josh','Justin'], index= [3,2,1])
+print(series2)
