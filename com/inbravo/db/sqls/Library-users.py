d569e15a675daaa2386fa13195b1b4e957d2cd88
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-03-26 16:18:45 +0530

    Update README.md

--- com/inbravo/db/sqls/Library-users.py
+++ com/inbravo/db/sqls/Library-users.py
@@ -1,4 +1,17 @@
-import
+import mysql.connector
+
+def start(): connectToMySQL()
+
+def connectToMySQL():
+    print("--------------------------------------------------------")
+    print(" -*- Connecting to MySQL DB  -*- ")
+    print("--------------------------------------------------------")
+    cnx = mysql.connector.connect(user='root', password='pilkhuwa01!', host='127.0.0.1', database='default_db')
+    df = pd.read_sql_query('select * from pet', cnx)
+    print(df)
+    cnx.close()
+
+start()
 
 print('Welcome to the Library! What do you want to do?')
 print('1. Add Member')
@@ -10,6 +23,6 @@ choice = int(input('Please enter the index number of your choice: '))
 if choice == 1:
     username = input('Please enter the name of the soon to be Member: ')
     email= input('Please enter the e-mail of the soon to be Member: ')
-    passsword= input('Please enter the preferred password of the soon to be Member: ')
+    password= input('Please enter the preferred password of the soon to be Member: ')
 
 elif choice == 2:
