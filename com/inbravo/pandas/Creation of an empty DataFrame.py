7031ebc6d433638ef08d84c1a3d5e8cdd7a640d6
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-03-26 16:37:59 +0530

    Update README.md

--- com/inbravo/pandas/Creation of an empty DataFrame.py
+++ com/inbravo/pandas/Creation of an empty DataFrame.py
@@ -9,17 +9,6 @@ print('\n')
 
 
 
-dictLst= {
-    'Name':['Jack','John','Zack'],
-    'Age':[14,14,15],
-    'Class':[12,12,12]
-}
-dFrameDictLst= pd.DataFrame(dictLst)      # Creation of DataFrame from dictionary of lists
-print(dFrameDictLst)
-print('\n')
-dFrameDictLst1= pd.DataFrame(dFrameDictLst, columns= ['Age', 'Class', 'Name'])        # To change the sequence of column names
-print(dFrameDictLst1)
-print('\n')
 
 series1= pd.Series([1,2,3,4,5], index= ['a','b','c','d','e'])
 series2= pd.Series([100,2000,-60,-8000, 700], index= ['a','b','c','d','e'])
