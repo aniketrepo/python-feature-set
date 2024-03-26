56b094d99085842ac0e13971d1c8e374261a47ff
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-03-26 16:23:13 +0530

    Update README.md

--- com/inbravo/pandas/Creation of an empty DataFrame.py
+++ com/inbravo/pandas/Creation of an empty DataFrame.py
@@ -6,12 +6,7 @@ dframeEmt= pd.DataFrame()    # Creation of an empty DataFrame
 print(dframeEmt)
 print('\n')
 
-array1= np.array([10,20,30])
-array2= np.array([100,200,300])
-array3= np.array([-10,-20,-30,-40])
-dframeNpy= pd.DataFrame([array1, array2, array3], columns= ['a','b','c','d'])   # Creation of a DataFrame from a NumPy ndarrays
-print(dframeNpy)
-print('\n')
+
 
 dict= [
     {'a':10, 'b':20},
