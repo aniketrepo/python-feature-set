bf30dabdb60b4e99a942a1ca05e53d7b0a98601b
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-03-26 16:41:59 +0530

    Update README.md

--- com/inbravo/pandas/Creation of an empty DataFrame.py
+++ com/inbravo/pandas/Creation of an empty DataFrame.py
@@ -4,22 +4,3 @@ import numpy as np
 # Creation of DataFrame-
 dframeEmt= pd.DataFrame()    # Creation of an empty DataFrame
 print(dframeEmt)
-print('\n')
-
-
-
-
-
-series1= pd.Series([1,2,3,4,5], index= ['a','b','c','d','e'])
-series2= pd.Series([100,2000,-60,-8000, 700], index= ['a','b','c','d','e'])
-series3= pd.Series([10,20,50,90,70], index= ['x','y','a','c','e'])
-
-dFrame1= pd.DataFrame(series1)
-print(dFrame1)
-print('\n')
-dFrame2= pd.DataFrame([series1 + series2])
-print(dFrame2)
-print('\n')
-dFrame3= pd.DataFrame(series3)
-print(dFrame3)
-dFrame3= pd.DataFrame([series3 * series2])
