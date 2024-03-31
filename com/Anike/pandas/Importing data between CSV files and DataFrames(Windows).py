3a591e5c095eefa3aba95810e9eb50e339c8025b
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-03-31 23:15:13 +0530

    changes

---
+++ com/Anike/pandas/Importing data between CSV files and DataFrames(Windows).py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+marks = pd.read_csv('C:/Users/Administrator/Documents/GitHub/python-feature-set/com/Anike/pandas/CSVs/ResultData(Windows).csv', sep=',', header=0)
+print(marks)
--- com/Anike/pandas/Importing data between CSV files and DataFrames.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-marks = pd.read_csv('C:/Users/Administrator/Documents/GitHub/python-feature-set/com/Anike/pandas/CSVs/ResultData.csv', sep=',', header=0)
-print(marks)
