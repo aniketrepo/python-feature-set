6d8b9fecb3ae2620956ef5c4067e67766b7bb138
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-04-04 22:34:48 +0530

    Tip Calculator

--- com/Anike/100-days-of-coding/Tip Calculator.py
+++ com/Anike/100-days-of-coding/Tip Calculator.py
@@ -1,5 +1,11 @@
 bill = int(input('Hello! I am a tip calculator. Please enter the total bill: $'))
-percentage = int(input('How much tip would you like to give, 10%, 12% or 15%?\n'))
+tip = int(input('How much tip would you like to give, 10%, 12% or 15%?\n'))
 people = int(input('Please enter the number of people who have to split the bill: '))
-tip = ((int(bill) / 100) * percentage) / people
-print(f'Each person should pay an amount of ${tip}. Have a great day!')
+
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amount = round(bill_per_person, 2)
+
+print(f'Each person should pay an amount of ${final_amount}. Have a great day!')
