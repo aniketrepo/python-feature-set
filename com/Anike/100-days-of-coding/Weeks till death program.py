ae851082665385d6929b8fadd8814c5fe47d77ae
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-04-04 22:11:35 +0530

    Weeks till death program

---
+++ com/Anike/100-days-of-coding/Weeks till death program.py
@@ -0,0 +1,5 @@
+# Lets say that each one of us has 90 years left to live
+age = int(input('Please enter your current age: '))
+years = 90 - age
+weeks = years * 52
+print(f'You have {weeks} weeks left to live. Use your remaining time wisely.')
