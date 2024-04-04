e6b2e1799d7f80cb85ab94f9b23ef035a254cb98
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-04-04 18:06:46 +0530

    Number Manipulation and F Strings in Python

---
+++ com/Anike/100-days-of-coding/Number Manipulation and F Strings in Python.py
@@ -0,0 +1,15 @@
+# Number Manipulation
+print(3 + 5)
+print(3 - 5)
+print(3 * 5)
+print(3 / 5)
+print(3 // 5)
+print('\n')
+
+# F Strings
+age = 18
+percentage = 95
+standard = '12th'
+name = 'Aniket Dixit'
+
+print(f'{name}, age {age} years old, has scored {percentage}% in {standard} standard. ')
