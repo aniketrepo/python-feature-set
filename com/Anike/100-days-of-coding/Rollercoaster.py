c15cad36936baabce844e0f5f2ec63c0881fa70e
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-04-09 16:09:10 +0530

    changes

---
+++ com/Anike/100-days-of-coding/Rollercoaster.py
@@ -0,0 +1,7 @@
+print('Welcome to the rollercoaster!')
+height = int(input('Please enter your height in cm: '))
+
+if height >= 120:
+    print('You are tall enough to go to the roller coaster, you may proceed.')
+else:
+    print('Ha, SHORT!')
