8c231346f45efaef428908a29c225d207930d35b
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-04-17 17:25:39 +0530

    changes

--- com/Anike/100-days-of-coding/Rollercoaster v5.py
+++ com/Anike/100-days-of-coding/Rollercoaster v5.py
@@ -18,7 +18,7 @@ if height >= 120:
     #for picture
     choice = str(input("Pictures cost an additional of $3. Do you want a picture taken (Y or N): "))
     if choice == "Y" or "y":
-        bill = bill + 3
+        bill += 3
         print(f"Your total bill is {bill}.")
     else:
         print(f"Your total bill is {bill}.")
