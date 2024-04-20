055ff4a77ce3773fd0cdf3ae4029a5b597fb7cbe
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-04-20 14:13:04 +0530

    changes

--- com/Anike/100-days-of-coding/Leap Year.py
+++ com/Anike/100-days-of-coding/Leap Year.py
@@ -1,13 +1,13 @@
 # [https://github.com/aniketrepo/python-feature-set/blob/main/com/Anike/stuff/Untitled.png] used for reference
-year = int(input('Please enter a year of your choice: '))
+year = int(input("Please enter a year of your choice: "))
 
 if year % 4 == 0:
   if year % 100 == 0:
     if year % 400 == 0:
-      print('The year you gave is a leap year')
+      print("The year you gave is a leap year")
     else:
-      print('The year you gave is not a leap year')
+      print("The year you gave is not a leap year")
   else:
-    print('The year you gave is leap year')
+    print("The year you gave is leap year")
 else:
-  print('The year you gave is not a leap year')
+  print("The year you gave is not a leap year")
