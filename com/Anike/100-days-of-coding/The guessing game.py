437af4d0932db041cca96f02ca75c401f9d37776
Aniket Dixit <64648557+aniketrepo@users.noreply.github.com>
2024-04-27 22:35:01 +0530

    changes

--- com/Anike/100-days-of-coding/The guessing game.py
+++ com/Anike/100-days-of-coding/The guessing game.py
@@ -5,17 +5,16 @@ rng = int(input("Please enter the range (from 1 to): "))
 
 print(f"Alright! Generating a number between 1 and {rng}!")
 to_guess = random.randint(1, rng)
-
 attempts = 0
 
 while True:
     guessed = int(input("Please enter your guess: "))
     attempts += 1
     if guessed > to_guess:
-        print("Too high! Try again.")
+        print("Too high! Try again.\n")
     elif guessed < to_guess:
-        print("Too low! Try again.")
+        print("Too low! Try again.\n")
     else:
-        print("Congratulations! You guessed the correct number!")
+        print("Congratulations! You guessed the correct number!\n")
         break
 print(f"You guessed the correct number in {attempts} attempts.")
